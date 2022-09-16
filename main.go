f5a2495ff50278b417a0b0bfb7e0e6330c488869
Lucas Resende <lucasresone@gmail.com>
2022-09-16 08:24:53 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
 import "fmt"
+import "flag"
 
 func main() {
 	fmt.Println("Hello My Little Friend")
+}
+
+func init() {
+	test := ""
+	flag.StringVar(&test, "t", "", "test")
+	flag.Parse()
+	
+	fmt.Println(test)
 }
